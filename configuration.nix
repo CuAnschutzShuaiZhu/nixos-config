fe7ee363cf76654c17ac8b0fdea8fe53d59d7f21
CuAnschutzShuaiZhu <shuai.zhu@cuanschutz.edu>
2025-05-06 17:01:29 -0600

    add zsh and starship to home.nix

--- configuration.nix
+++ configuration.nix
@@ -87,10 +87,11 @@
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
       kdePackages.kate
-    #  thunderbird
     ];
+    shell = pkgs.zsh;
   };
-
+  
+  programs.zsh.enable = true;
   # Install firefox.
   programs.firefox.enable = true;
   # Vscode remote extension
@@ -99,6 +100,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.  
+
   ];
 
 
